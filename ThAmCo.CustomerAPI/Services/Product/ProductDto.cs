a7c6bae4b8ab2cfeb4b0edf9da15a0c88172c218
Amaan <amaanaliakhtar@gmail.com>
2023-12-16 11:29:01 +0000

    Updated customer dto

--- ThAmCo.CustomerAPI/Services/Product/ProductDto.cs
+++ ThAmCo.CustomerAPI/Services/Product/ProductDto.cs
@@ -6,7 +6,14 @@ public class ProductDto
 {
     //these must have get and set to be serializable to json
     public int Id { get; set; }
+    public string? Ean {get; set; }
+    public int CategoryId { get; set; }
+    public string? CategoryName {get; set; }
+    public int BrandId { get; set; }
+    public string? BrandName {get; set; }
     public string? Name { get; set; }
+    public string? Description {get; set; }
     public double Price { get; set; }
-    public int Stock { get; set; }
+    public bool InStock { get; set; }
+    public DateTime? ExpectedRestock { get; set; }
 }
--- ThAmCo.CustomerAPI/Services/Product/ProductServiceFake.cs
+++ ThAmCo.CustomerAPI/Services/Product/ProductServiceFake.cs
@@ -6,11 +6,11 @@ public class ProductServiceFake : IProductService
 {
     private readonly ProductDto[] _products =
     {
-            new ProductDto { Id = 1, Name = "Nike Air Max 1", Price = 130.00, Stock = 1 },
-            new ProductDto { Id = 2, Name = "Nike Air Max 90", Price = 95.00, Stock = 4 },
-            new ProductDto { Id = 3, Name = "Adidas Ultraboost", Price = 120.00, Stock = 6 },
-            new ProductDto { Id = 4, Name = "Adidas NMD", Price = 100.00, Stock = 5 },
-            new ProductDto { Id = 5, Name = "New Balance 990", Price = 200.00, Stock = 13 }
+            new ProductDto { Id = 1, Name = "Nike Air Max 1", Price = 130.00, InStock = true },
+            new ProductDto { Id = 2, Name = "Nike Air Max 90", Price = 95.00, InStock = true },
+            new ProductDto { Id = 3, Name = "Adidas Ultraboost", Price = 120.00, InStock = true },
+            new ProductDto { Id = 4, Name = "Adidas NMD", Price = 100.00, InStock = false },
+            new ProductDto { Id = 5, Name = "New Balance 990", Price = 200.00, InStock = false }
         };
 
     public Task<ProductDto?> GetProductAsync(int id)
