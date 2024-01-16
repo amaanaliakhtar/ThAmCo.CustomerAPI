1d1db961afd48e6fb4013b6bfd42ac05e0ead861
Amaan <amaanaliakhtar@gmail.com>
2024-01-16 09:09:21 +0000

    Test commit

--- ThAmCo.CustomerAPI/Services/Product/ProductService.cs
+++ ThAmCo.CustomerAPI/Services/Product/ProductService.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Net;
 
-namespace ThAmCo.CustomerAPI.Services.Product;
+namespace ThAmCo.CustomerAPI.Services.Product;      //Test deployment
 
 public class ProductService : IProductService
 {
